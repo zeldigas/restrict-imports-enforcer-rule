3e567d365af2f1c4731ae227f190c3d9b7c48757
Simon Taddiken <Simon.Taddiken@fincon.eu>
2019-02-06 13:14:33 +0100

    Improve jdk tests

--- src/main/java/de/skuzzle/enforcer/restrictimports/analyze/LineSupplier.java
+++
@@ -1,9 +0,0 @@
-package de.skuzzle.enforcer.restrictimports.analyze;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.stream.Stream;
-
-interface LineSupplier {
-    Stream<String> lines(Path path) throws IOException;
-}
